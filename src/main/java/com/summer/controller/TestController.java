e5830c23a92480b68c85293fa072ca9bb02ce06e
xia45399 <453993887@qq.com>
2018-11-14 20:45:42 +0800

    添加fastJson Message Converter 可以返回long map等类型

--- src/main/java/com/summer/controller/TestController.java
+++ src/main/java/com/summer/controller/TestController.java
@@ -3,6 +3,9 @@ package com.summer.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/cgi/test")
 public class TestController {
@@ -10,5 +13,19 @@ public class TestController {
     @RequestMapping("test1")
     public Object test() {
         return "a";
+    }
+
+    @RequestMapping("test2")
+    public Object test2(Long a) {
+        return a;
+    }
+
+    @RequestMapping("test3")
+    public Object test3() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("a", "a");
+        map.put("b", "啊");
+        map.put("c", 1);
+        return map;
     }
 }
