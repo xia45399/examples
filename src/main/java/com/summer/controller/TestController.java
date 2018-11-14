fe865a5511418d1e3866486b278d6ffee86c46f4
xia45399 <453993887@qq.com>
2018-11-14 23:20:26 +0800

    配置数据库连接池

--- src/main/java/com/summer/controller/TestController.java
+++ src/main/java/com/summer/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.summer.controller;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,8 +28,20 @@ public class TestController {
     public Object test3() {
         Map<String, Object> map = new HashMap<>();
         map.put("a", "a");
-        map.put("b", "啊");
+        map.put("b", "中文");
         map.put("c", 1);
         return map;
+    }
+
+    @Resource
+    private DruidDataSource druidDataSource;
+
+    @RequestMapping("testDbPool")
+    public Object testDbPool() throws SQLException {
+        Connection con = druidDataSource.getConnection();
+        long closeCount = druidDataSource.getCloseCount();
+        con.close();
+        long count = druidDataSource.getConnectCount();
+        return count + "=" + closeCount;
     }
 }
