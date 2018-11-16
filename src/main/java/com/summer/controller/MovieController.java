8184a34601c154c75d6844accc291488d118f5f0
xia45399 <453993887@qq.com>
2018-11-16 19:27:13 +0800

    数据的增删改查接口

--- src/main/java/com/summer/controller/MovieController.java
+++ src/main/java/com/summer/controller/MovieController.java
@@ -1,11 +1,12 @@
 package com.summer.controller;
 
-import com.summer.pojo.Movie;
 import com.summer.dao.MovieMapper;
+import com.summer.pojo.Movie;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("controller/movie")
@@ -19,5 +20,24 @@ public class MovieController {
         movie.setName("测试");
         movie.setAddTime(System.currentTimeMillis() / 1000);
         return movieMapper.insert(movie);
+    }
+
+    @RequestMapping("delete")
+    public Object delete(int id) {
+        return movieMapper.delete(id);
+    }
+
+    @RequestMapping("update")
+    public Object update() {
+        Movie movie = new Movie();
+        movie.setId(5);
+        movie.setName("测试修改5");
+        return movieMapper.update(movie);
+    }
+
+    @RequestMapping("list")
+    public Object list() {
+        List<Movie> list = movieMapper.list();
+        return list;
     }
 }
--- src/main/java/com/summer/dao/MovieMapper.java
+++ src/main/java/com/summer/dao/MovieMapper.java
@@ -2,6 +2,14 @@ package com.summer.dao;
 
 import com.summer.pojo.Movie;
 
+import java.util.List;
+
 public interface MovieMapper {
     int insert(Movie movie);
+
+    int delete(int id);
+
+    int update(Movie movie);
+
+    List<Movie> list();
 }
