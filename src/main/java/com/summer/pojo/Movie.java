cbb1f1d45fcae86fed9e7d5e819f8942647ddfd4
xia45399 <453993887@qq.com>
2018-11-16 20:29:58 +0800

    使用lombok精简代码

--- src/main/java/com/summer/pojo/Movie.java
+++ src/main/java/com/summer/pojo/Movie.java
@@ -1,31 +1,10 @@
 package com.summer.pojo;
 
+import lombok.Data;
+
+@Data
 public class Movie {
-    private Long id;
+    private Integer id;
     private String name;
     private Long addTime;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getAddTime() {
-        return addTime;
-    }
-
-    public void setAddTime(Long addTime) {
-        this.addTime = addTime;
-    }
 }
